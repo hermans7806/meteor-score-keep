c964481d7068def1053f550006a6662e46615d9e
Herman <herman.sutanto@reebonz.com>
2017-08-07 07:00:10 +0800

    moving to components

--- client/main.js
+++ client/main.js
@@ -6,47 +6,18 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
-
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={() => Players.update(player._id, {$inc: {score: -1}})}>-1</button>
-        <button onClick={() => Players.update(player._id, {$inc: {score: 1}})}>+1</button>
-        <button onClick={() => Players.remove(player._id)}>X</button>
-      </p>
-    );
-  });
-};
-
-const handleSubmit = (e) => {
-  let playerName = e.target.playerName.value;
-
-  e.preventDefault();
-
-  if (playerName) {
-    e.target.playerName.value = '';
-    Players.insert({
-      name: playerName,
-      score: 0
-    });
-  }
-};
+import PlayerList from './../imports/ui/PlayerList';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = 'Score Keep';
+    let subtitle = 'Created by Herman';
     let jsx = (
       <div>
-        <TitleBar/>
-        {renderPlayers(players)}
+        <TitleBar title={title} subtitle={subtitle}/>
+        <PlayerList players={players}/>
         <AddPlayer/>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Player name"/>
-          <button>Add Player</button>
-        </form>
       </div>
     );
     ReactDOM.render(jsx, document.getElementById('app'));
