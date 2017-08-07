ccf17f7de8679607b1f13ea7574d2ca5086c2c66
Herman Sutanto <herman.sutanto@reebonz.com>
2017-08-07 16:59:41 +0800

    Add Apps Component and sort data

--- client/main.js
+++ client/main.js
@@ -4,22 +4,12 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import PlayerList from './../imports/ui/PlayerList';
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
     let title = 'Score Keep';
-    let subtitle = 'Created by Herman';
-    let jsx = (
-      <div>
-        <TitleBar title={title} subtitle={subtitle}/>
-        <PlayerList players={players}/>
-        <AddPlayer/>
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
 });
