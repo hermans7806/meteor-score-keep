f51bbc7d73b912b202b61c4e94fff280f8648471
Herman <herman.sutanto@reebonz.com>
2017-08-08 06:59:13 +0800

    Add styles

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,12 +6,14 @@ import {Players} from './../api/players';
 export default class Player extends React.Component {
   render() {
     return (
-      <p key={this.props.player._id}>
-        {this.props.player.name} has {this.props.player.score} point(s).
-        <button onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>-1</button>
-        <button onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>+1</button>
-        <button onClick={() => Players.remove(this.props.player._id)}>X</button>
-      </p>
+      <div key={this.props.player._id} className="item">
+        <p>
+          {this.props.player.name} has {this.props.player.score} point(s).
+        </p>
+        <button className="button button--round" onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>-1</button>
+        <button className="button button--round" onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>+1</button>
+        <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>X</button>
+      </div>
     );
   }
 };
